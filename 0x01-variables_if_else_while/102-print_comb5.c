7db9fb7f7c84881a2a846857bb42b892dd0ca8e4
sonny97-gif <charitywachira18@gmail.com>
2023-03-20 12:42:24 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,30 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main- Prints all possible combinations of two two-digit numbers,
- *        ranging from 0-99, separated by a comma followed by a space.
+ * main - a simple program that outputs unordered
+ * combinations of two digit integers without duplicates
  *
- * Return: Always 0.
+ * Return: 0 on success
  */
 int main(void)
 {
-	int num1, num2;
+	int ifirst;
+	int i;
+	int jfirst;
+	int j;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (ifirst = 48; ifirst < 58; ifirst++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num++)
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (i = 48; i < 58; i++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			if (num1 == 98 && num2 ==  99)
-				continue;
-			putchar(',');
-			putchar(' ');
+			j = i + 1;
+			jfirst = ifirst;
+			for (; jfirst < 58; jfirst++)
+			{
+				for (; j < 58; j++)
+				{
+					putchar(ifirst);
+					putchar(i);
+					putchar(' ');
+					putchar(jfirst);
+					putchar(j);
+					if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				j = 48;
+			}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
+
