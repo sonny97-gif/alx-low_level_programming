acf249dd7419f8e58c03927d9df3492bdc5cd6aa
sonny97-gif <charitywachira18@gmail.com>
2023-03-20 11:23:10 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - print alphabet reversed using putchar
+ * Return: 0 always on success
+ */
+int main(void)
+{
+	char alpha = 'z';
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n);
+	putchar('\n');
+
+	return (0);
+}
