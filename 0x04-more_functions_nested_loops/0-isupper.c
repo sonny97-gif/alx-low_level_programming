51000dd731227cb234e5d9930615dd21aff1639a
sonny97-gif <charitywachira18@gmail.com>
2023-03-24 12:28:35 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - a function that checks for uppercase character
- * @c: input
- * Return: 1 if c is uppercase, 0 otherwise
+ * _isupper - checks for uppercase character
+ * @c: character that is checked
+ * Return: if uppercase returns 0 otherwise
  */
+
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
-
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,13 +3,18 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
-int largest_number(int a, int b, int c);
-
-void print_remaining_days(int month, int day, int year);
-
-int convert_day(int month, int day);
-
-#endif /* MAIN_H */
+#endif
 
